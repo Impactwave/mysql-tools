7e44d8d158a84270f0da99029c8cf742fd015621
Cláudio Silva <claudio.silva@impactwave.com>
2017-01-13 01:55:19 +0000

    FIX: incomplete backups.

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -115,8 +115,7 @@ ver=${BASH_REMATCH[1]}
 echo "Dumping database '$MAINDB'..."
 mkdir -p "$TMP_DIR"
 # Note: The PHP pipe script removes 'DEFINER=xxx' SQL statements to prevent the 'you need (at least one of) the SUPER privilege(s) for this operation' error.
-mysqldump -u $MYSQL_USER -h $HOST $BACKUP_OTIONS $MAINDB "$TABLES" | php -r "while (feof (STDIN) === false) echo preg_replace ('#DEFINER=[^\s\*]+#', '', fgets (STDIN));" > "$TMP_DIR/$MAIN"
-
+mysqldump -u $MYSQL_USER -h $HOST $BACKUP_OTIONS $MAINDB $TABLES | php -r "while (feof (STDIN) === false) echo preg_replace ('#DEFINER=[^\s\*]+#', '', fgets (STDIN));" > "$TMP_DIR/$MAIN"
 
 [ $? -ne 0 ] && exit 1
 
